2b2ffcd1c7cc27e8f6aa332af4c0ca1386c4de09
NJRFirstOfHisName <130861854+NJRFirstOfHisName@users.noreply.github.com>
2023-07-03 10:45:32 -0600

    Add some much-needed comments

--- src/index.js
+++ src/index.js
@@ -3,27 +3,31 @@ import printListItem from "./printListItem";
 import deleteEntry from "./deleteEntry";
 import ListItem from "./addListItem";
 
+// Dom elements used for and in event listeners
 const container = document.getElementById('container');
 const tdList = document.getElementById('tdList');
 const completedList = document.getElementById('completedList');
 const project = document.getElementById('projects');
 
-const defaultList = new ListItem("Default Item","This is a default item for defaulting","07-02-2023",false, "defaultProject");
-const defaultCompleted = new ListItem("Default completed Item","This is a completed default item for completed defaulting","01-012-2001",true, "defaultProject");
-const testList = new ListItem("Test Item","This is a test item for testing","09-11-2050", false, "testProject");
-const testCompleted = new ListItem("Test completed Item","This is a completed test item for completed testing","11-11-1111", true, "testProject")
+//Hard-coded entries to simplify testing, will be deleted once project is finished
+    const defaultList = new ListItem("Default Item","This is a default item for defaulting","07-02-2023",false, "defaultProject");
+    const defaultCompleted = new ListItem("Default completed Item","This is a completed default item for completed defaulting","01-012-2001",true, "defaultProject");
+    const testList = new ListItem("Test Item","This is a test item for testing","09-11-2050", false, "testProject");
+    const testCompleted = new ListItem("Test completed Item","This is a completed test item for completed testing","11-11-1111", true, "testProject")
 
-defaultCompleted.completed = true;
-testCompleted.completed = true;
+    defaultCompleted.completed = true;
+    testCompleted.completed = true;
 
-const fullList = [defaultList, testList, defaultCompleted, testCompleted];
+    const fullList = [defaultList, testList, defaultCompleted, testCompleted];
 
+//Prints contents of default project on page load
 fullList.forEach(ListItem => {
     if(ListItem.getProject === project.value){
         printListItem(ListItem);
     }
 })
 
+//Prints contents of selected project when selection is changed
 project.addEventListener('change', () => {
     tdList.innerHTML = "";
     completedList.innerHTML = "";
@@ -34,6 +38,7 @@ project.addEventListener('change', () => {
     })
 })
 
+//Creates a new, uncompleted task when 'Add' button is pressed IF title is present
 const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', () => {
     const title = document.getElementById('title').value;
@@ -47,10 +52,13 @@ addBtn.addEventListener('click', () => {
     }
 })
 
+//Listens for any clicks within the tasks container.
 container.addEventListener('click', (e) => {
+    //If delete button is clicked, deletes task.
     if(e.target.classList.contains('delete')) {
         e.target.parentNode.remove();
     }
+    //If checkbox is clicked, moves task from 'tasks' to 'completed' or vice-versa
     if(e.target.classList.contains('check')) {
         const labelChange = e.target.nextElementSibling;
         if(e.target.checked) {
--- src/printListItem.js
+++ src/printListItem.js
@@ -2,11 +2,13 @@ export default function printListItem(ListItem) {
     const newEntry = document.createElement('div');
     newEntry.className = "entry";
 
+    //Create checkbox for marking tasks as complete (or un-marking)
     const checkbox = document.createElement('input');
     checkbox.type = "checkbox";
     checkbox.className = "check";
     newEntry.appendChild(checkbox);
 
+    //Create label with the task's title and highlights it if it's a priority
     const newLabel = document.createElement('label');
     newLabel.innerHTML = ListItem.getTitle;
     if(ListItem.getPriority){
@@ -14,22 +16,25 @@ export default function printListItem(ListItem) {
     }
     newEntry.appendChild(newLabel);
 
+    //Add description. If no description is supplied field will be blank
     const descriptionText = document.createElement('p');
     descriptionText.innerHTML = ListItem.getDescription;
     newEntry.appendChild(descriptionText);
 
+    //Add due date. If no date is supplied field will be blank
     const dueDate = document.createElement('p');
     dueDate.innerHTML = ListItem.getDueDate;
     newEntry.appendChild(dueDate);
 
+    //Add button to delete task
     const deleteBtn = document.createElement('button');
     deleteBtn.innerHTML = "Delete";
     deleteBtn.className = "delete";
     newEntry.appendChild(deleteBtn);
 
+    //Adds task to appropriate div depending on whether it's marked complete
     if(!ListItem.getCompleted){
         console.log(ListItem.getCompleted)
-
         const tdList = document.getElementById('tdList');
         tdList.appendChild(newEntry);
     } else {
