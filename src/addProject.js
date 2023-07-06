c2e56369e8541ff507fbadcfd81aa54e6591fa52
NJRFirstOfHisName <130861854+NJRFirstOfHisName@users.noreply.github.com>
2023-07-06 10:57:43 -0600

    Fix addProject to properly display and check new projects against existing

--- src/addProject.js
+++ src/addProject.js
@@ -1,19 +1,19 @@
 export default function addProject(project) {
-    //First prompts the user for the new project name and checks that it's not the same as an existing project
-    let newProjectName = prompt("Enter the name of your new project:") + "Project";
+    let name = document.getElementById("projectInput").value  + "Project";
+    let valid = true;
     for(let i=0 ; i<project.options.length ; i++) {
         let existingProject = project.options[i].value.toLowerCase();
-        if (newProjectName.toLowerCase() === existingProject) {
+        if (name.toLowerCase() === existingProject) {
             alert("A project already exists with that name. Please select a unique project name.");
-            newProjectName = "";
+            valid = false;
         }
     }
 
     //Creates new project with supplied name and adds it to the select box
-    if(newProjectName) {
+    if(valid) {
         let newProject = document.createElement('option');
-        newProject.value = newProjectName;
-        newProject.text = newProjectName.slice(0, -7);
+        newProject.value = name;
+        newProject.text = name.slice(0, -7);
         project.add(newProject);
     }
 }
